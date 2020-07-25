6d7f3b3c00322d1977ca8252ad55d60a7c5bf205
amandeepmicro <55043082+amandeepmicro@users.noreply.github.com>
2020-07-25 20:17:40 +0100

    initial setup and markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hh</h1>
+      {/* Header */}
+      {/* {Title + Select Input dropdown} */}
+      
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
