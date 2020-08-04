5169781e1298cd74accadf6122c2ee913a8b0c10
amandeepmicro <55043082+amandeepmicro@users.noreply.github.com>
2020-08-04 18:24:07 +0100

    flags added in table

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,18 +4,20 @@ import '../css/Table.css';
 
 function Table({countries}) {
 
-     
+     console.log(countries)
     return (
         <div className="table">      
            
             <tr className="tableHead">
+                <td>Flag</td>
                 <td>Country</td>
                 <td>Total Cases</td>
             </tr>
-            {countries.slice(0,10).map(({country,cases},index) =>{
+            {countries.slice(0,10).map(({countryInfo,country,cases},index) =>{
                 return(
                     
                 <tr key={index}>
+                    <td><img width="20px"src={countryInfo.flag}/></td>
                     <td>{country}</td>
                     <td><strong>{numeral(cases).format("0,0")}</strong></td>
                 </tr>
